ffb11ae9cbf401a5108e1c3dcdd00e852fa39795
Ashley-Mondala <sweetashyy@gmail.com>
2026-09-23 16:00:29 -0700

    updated players class to keep track of what stage their on.

--- creating_player.py
+++ creating_player.py
@@ -9,27 +9,27 @@ def player_setter():
     match player_class.lower():
         case ClassType.WARRIOR:
             warrior = ClassStats(attack_damage=55, accuracy=70, health=110, speed=55, defense=100)
-            user = Player(name=name, class_type=ClassType.WARRIOR, stats=warrior)
+            user = Player(name=name, class_type=ClassType.WARRIOR, stats=warrior, stage = 0)
             return user
         
         case ClassType.RANGER:
             ranger = ClassStats(attack_damage=70, accuracy=75, health=70, speed=70, defense=40)
-            user = Player(name=name, class_type=ClassType.RANGER, stats=ranger)
+            user = Player(name=name, class_type=ClassType.RANGER, stats=ranger, stage = 0)
             return user
         
         case ClassType.ASSASSIN:
             assassin = ClassStats(attack_damage=75, accuracy=80, health=60, speed=80, defense=30)
-            user = Player(name=name, class_type=ClassType.ASSASSIN, stats=assassin)
+            user = Player(name=name, class_type=ClassType.ASSASSIN, stats=assassin, stage = 0)
             return user
         
         case ClassType.WIZARD:
             wizard = ClassStats(attack_damage=75, accuracy=80, health=70, speed=50, defense=50)
-            user = Player(name=name, class_type=ClassType.WIZARD, stats=wizard)
+            user = Player(name=name, class_type=ClassType.WIZARD, stats=wizard, stage = 0)
             return user
         
         case ClassType.DRUID:
             druid = ClassStats(attack_damage=60, accuracy=60, health=90, speed=60, defense=80)
-            user = Player(name=name, class_type=ClassType.DRUID, stats=druid)
+            user = Player(name=name, class_type=ClassType.DRUID, stats=druid, stage = 0)
             return user
         
         case _:
--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 from class_type_stats import ClassStats, ClassType
 
 class Player():
-    def __init__(self, name: str, class_type: ClassType, stats: ClassStats):
+    def __init__(self, name: str, class_type: ClassType, stats: ClassStats, stage: int):
         self.name = name
         self.class_type = ClassType
         self.stats = stats
+        self.stage = stage
     
     def get_stats(self):
         attack_damage = self.stats.attack_damage
